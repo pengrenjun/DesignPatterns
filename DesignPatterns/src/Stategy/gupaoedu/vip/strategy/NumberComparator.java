fd6171eb852aaa5f8e57e050f19d7c1afed8d58c
pengrj <pengrj@emrubik.com>
2019-01-24 16:40:34 +0800

    策略模式

---
+++ DesignPatterns/src/Stategy/gupaoedu/vip/strategy/NumberComparator.java
@@ -0,0 +1,10 @@
+package Stategy.gupaoedu.vip.strategy;
+
+public class NumberComparator implements Comparator{
+
+	@Override
+	public int compareTo(Object obj1, Object obj2) {
+		return 0;
+	}
+
+}
---
+++ DesignPatterns/src/Stategy/gupaoedu/vip/strategy/ObjectComparator.java
@@ -0,0 +1,10 @@
+package Stategy.gupaoedu.vip.strategy;
+
+public class ObjectComparator implements Comparator{
+
+	@Override
+	public int compareTo(Object obj1, Object obj2) {
+		return 0;
+	}
+
+}
