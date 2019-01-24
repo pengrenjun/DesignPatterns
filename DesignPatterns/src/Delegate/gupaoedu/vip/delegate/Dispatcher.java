55c69a99c2c71b66fcfc05cdb1ce82dbcb8c5f5a
pengrj <pengrj@emrubik.com>
2019-01-24 16:37:51 +0800

    委派模式

---
+++ DesignPatterns/src/Delegate/gupaoedu/vip/delegate/Dispatcher.java
@@ -0,0 +1,18 @@
+package Delegate.gupaoedu.vip.delegate;
+
+public class Dispatcher implements IExector{
+	IExector exector;
+	
+	Dispatcher(IExector exector){
+		this.exector = exector;
+	}
+	
+	
+	//项目经理，虽然也有执行方法
+	//但是他的工作职责是不一样的
+	@Override
+    public void doing() {
+		this.exector.doing();
+	}
+
+}
